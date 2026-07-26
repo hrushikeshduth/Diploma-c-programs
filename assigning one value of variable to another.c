534b571ff0fc3ae8bf32a050cf24d9b71265e400
hrushikeshduth <keshh7401@gmail.com>
2026-07-26 16:49:33 +0530

    Add files via upload

---
+++ assigning one value of variable to another.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main() 
+{
+  int num1 = 76,num2 = 49 ;
+
+num1=num2;
+
+// Here i assigned value of num2 to number 1 similarly we can assign value of num1 to num2 by changing their places
+
+printf("the number 1: %d",num1);
+
+return 0;}
+	
