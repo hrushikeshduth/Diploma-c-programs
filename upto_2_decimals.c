4f00d8e73b12ba2248035aa79c71acf1f1d2dbfa
hrushikeshduth <keshh7401@gmail.com>
2026-07-28 21:19:32 +0530

    Add files via upload

---
+++ upto_2_decimals.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+  double a , b;
+  printf("enter the values");
+  
+ 
+  scanf("%lf%lf", &a , &b);
+  
+  printf("%.2lf\n",a);
+  printf("%.2lf\n",b);
+  
+  return 0;
+
+}
