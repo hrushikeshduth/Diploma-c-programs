c925d8668c794613df5ffea405f4fb95d759939a
hrushikeshduth <keshh7401@gmail.com>
2026-07-30 22:40:06 +0530

    Add files via upload

---
+++ loop sum of 10 numbers .c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+
+{
+int sum;
+sum = 0;
+ 
+for( int i = 1 ; i<=10; i++)
+//here we used the loop for and we used tge counter value 1 beacuse it is 0 the first will any way be 0 no use so i negkected and atarted counted frok 1
+{
+	
+ 
+
+sum+=i;
+
+printf("\nthe total sum is: %d", sum);
+}
+
+return 0;
+
+
+
+
+
+
+	
+}
