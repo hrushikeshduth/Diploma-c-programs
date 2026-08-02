aaac9196f9b78c326f0093ce72bf3c4a906d4371
hrushikeshduth <keshh7401@gmail.com>
2026-08-02 20:19:01 +0530

    Add files via upload

---
+++ ssc_grading_system.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+  double grade;
+printf("enter the grade point");
+scanf("%lf", &grade);
+
+if(grade==10.0 || grade==9.8)
+{ printf("congrats your grade Is A1");
+}
+else if(grade>= 9)
+{
+  printf("oh good grade but try harder next time ur grade is A2");
+  }
+ else
+  {
+    printf("your child is very poor at studies");
+    
+    }
+    
+    return 0;
+    }
+
+
+  
