62c7160cb4862b07db133c8f891edcb1f949698a
hrushikeshduth <keshh7401@gmail.com>
2026-08-03 22:05:22 +0530

    Create_Numbers_from_1_to_10.c

---
+++ Numbers_from_1_to_10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+  int i;
+
+for(i=1;i<=10;i++)
+//here we are running for loop its syntax is for(value;intilization;++or--)
+{
+  // now loop will run 10 times as the counter like first loop prints 1secind prints 2 thrid 3 so on till it reaches 11 it stops bevause it dosent meet condition 
+
+
+printf("%d\n",i);
+  
+}
+  
+  return 0;
+  
+  }
+
