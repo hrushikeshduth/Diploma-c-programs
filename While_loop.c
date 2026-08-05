68a6f57f3160a4fe4347596f86eec065c1ced466
hrushikeshduth <keshh7401@gmail.com>
2026-08-05 21:31:58 +0530

    Create While_loop.c

---
+++ While_loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int counter = 1;
+while(counter<=10)//while loop syntax is also same but we can write only one condition inside tge bracets i dont know but its my hypothesis 
+//here it will check condition if it is true then ut will print the body if not it will skip
+{
+  printf("\nthe counter is running %d",counter);
+  counter++;
+  }
+  
+  
+  return 0;
+  }
+  
+
+
