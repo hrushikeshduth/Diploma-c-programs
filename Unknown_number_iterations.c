c288fa57f99183fab4476f612485b16b20ab4fd5
hrushikeshduth <keshh7401@gmail.com>
2026-08-07 22:20:27 +0530

    Create Unknown_number_iterations.c

---
+++ Unknown_number_iterations.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int number;
+printf("enter a number");
+scanf("%d",&number);
+while(number >= 0){
+  printf("yessssss the number is positive");
+  }
+    printf("book the number is negative");
+    
+   return 0;}
