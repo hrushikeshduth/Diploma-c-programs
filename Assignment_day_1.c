b9ac797b905dbf43ae15d21a63ac9dd62456fb2f
hrushikeshduth <keshh7401@gmail.com>
2026-08-13 23:23:29 +0530

    Create Assignment_day_1.c

---
+++ Assignment_day_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+float a,b,product;
+//herre i declared 3 varibles a b will be multolied the value of ab will be stored in product
+printf("enter the values");
+scanf("%f %f",&a,&b);
+product=a*b;
+printf("the product is %.3f\n",product);
+return 0;
+}
