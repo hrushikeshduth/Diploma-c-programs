278a363328ce551046c2d2ef8cfc67f7d9ced7c2
hrushikeshduth <keshh7401@gmail.com>
2026-08-14 23:49:23 +0530

    Create Day_2_daily_goal.c

---
+++ Day_2_daily_goal.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+
+ float f,c;
+printf("enter the temperature");
+scanf("enter the %f",&f);
+
+c =(f - 32) * 5 / 9;
+
+printf("the value celcius %f",c);
+
+return 0;
+}
