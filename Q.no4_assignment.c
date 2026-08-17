8c0bf8e0175ad9e1dd53278c47c39c7585a622d8
hrushikeshduth <keshh7401@gmail.com>
2026-08-17 20:44:58 +0530

    Create Q.no4_assignment.c

---
+++ Q.no4_assignment.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+int a,b,c,sum;
+printf("enter a three digit number");
+scanf("%d%d%d",&a,&b,&c);
+sum=a+b+c;
+printf("the sum of three digits is %d",sum);
+return 0;
+}
