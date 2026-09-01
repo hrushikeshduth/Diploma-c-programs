97e8beabe124543ff17be9785a6215d5279c087b
hrushikeshduth <keshh7401@gmail.com>
2026-09-01 23:21:07 +0530

    Add files via upload

---
+++ float_return_value_function.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+float add(float  x,float y){
+	
+	
+	float result=x+y;
+	return result;
+	
+	
+	
+	
+}
+
+
+
+
+int main(){
+float result = add(3.4f,4.6f);
+	
+
+
+printf("the value of result is %.2f",result);
+	
+	
+	
+return 0;
+
+
+
+
+}
