9edd9fd37776d4327e2d66723becd8c5759e615f
hrushikeshduth <keshh7401@gmail.com>
2026-09-10 21:22:29 +0530

    Add files via upload

---
+++ take_inputs_for_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+//take input store integers in array
+int main()
+{
+int array[4]={};
+printf("enter the values of array size is 4 which means 5 integers\n");
+
+for(int i=0;i<=4;i++){
+
+scanf(" %d",&array[i]);
+	
+
+
+}
+
+
+
+for(int i=0;i<=4;i++){
+printf(" the array is :%d",array[0,1,2,3,4]);	
+
+}	
+	
+	
+	
+	
+return 0;
+	
+	
+	
+}
