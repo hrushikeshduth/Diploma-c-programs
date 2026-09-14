0d7956eb0d20e63fb46044ede747bd8aae897730
hrushikeshduth <keshh7401@gmail.com>
2026-09-14 23:46:03 +0530

    Add files via upload

---
+++ reversing_a_number.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(){
+int num,i=10,digit;
+num = 1234;
+1
+while(num>0){
+digit=num%i;
+printf("%d",digit);
+num=num/i;
+
+	
+	
+	
+	
+	
+	
+	
+	
+}
+return 0;
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
