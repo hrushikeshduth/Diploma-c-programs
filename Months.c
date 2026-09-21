3b36dca2e6f5dbd347b25c657be78ddcd9b9f29f
hrushikeshduth <keshh7401@gmail.com>
2026-09-21 23:47:34 +0530

    Create Months.c

---
+++ Months.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+int main(){
+int days,YEAR;
+int J1,F1,F2,M1,A,M,J,JU,AU,S,O,
+N,D;
+J1=31;
+F1=28;
+F2=27;
+M1=31;
+A=30;
+M=31;
+J=30;
+JU=31;
+AU=31;
+S=30;
+O=31;
+N=30;
+D=31;
+char month='\0';
+printf("ENTER YOUR CHOSEN MONTH\n");
+scanf(" %c",&month);
+switch(month){
+case 'J':
+printf("it is jan it has %d",J);
+break;
+	
+	
+	
+}
+printf("CHOOSE THE YEAR");
+scanf("%d",&YEAR);
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+}
